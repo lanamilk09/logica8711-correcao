fae08bd95f29fb46f87d6956beea80da1e5ea84e
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-03-30 21:50:47 -0300

    +=

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ int main(){
 
     int a = 10; 
    
-   std::cout<<++a<<std::endl;
-   std::cout<<++a<<std::endl;
+  a += 5; 
+  //a = a + 5 
+
+  std::cout<<a<<std::endl;
+  
    
     return 0; 
 
