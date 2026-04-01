654e720e8dc9d45c089bba387edf48e2c902aa92
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 20:23:34 -0300

    first code

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 #include<string>
 int main(){
 
-  char letra = 'E';
+ std::string nome;
+ int idade;
+ int anoAtual = 2026;
+ int result;
 
-  std::cout<<letra<<std::endl; 
-   
+ std::cout<<" Qual seu nome? "<<std::endl; 
+ std::cin>>nome;
+ std::cout<<" Qual sua idade? "<<std::endl; 
+ std::cin>>idade; 
+
+ result = anoAtual - idade; 
+ std::cout<<result<<std::endl;
+
+ std::cout<<"olá"<<nome<<"!!"<<std::endl;
+ std::cout<<"você tem"<<idade<<std::endl;
+ std::cout<<"você nasceu no ano"<<result; 
+
+
     return 0; 
 
 }
