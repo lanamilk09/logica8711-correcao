dcc05a061364b4e2d02c153eeec08c09ff085bf3
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 19:48:03 -0300

    auto

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include<string>
 int main(){
 
-    bool condicao = true;
-    //usado para saber se é verdadeiro 
+   auto num1 = 10;
+   auto num2 = 15.80f;
+   auto letra = 'E';
+   auto texto = "Alana";
 
-    bool condicao1 = false; 
-
-    std::cout<<condicao<<std::endl; 
-    std::cout<<condicao1<<std::endl; 
-
-
+   std::cout<<num1<<std::endl;
+   std::cout<<num2<<std::endl;
+   std::cout<<letra<<std::endl;
+   std::cout<<texto<<std::endl; 
+   
    
     return 0; 
 
