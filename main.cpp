18bedb1bf4387c219b94e632831672edac6244e8
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 21:12:09 -0300

    procedência

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
 #include<iostream>
-#include<string>
+
 int main(){
 
- std::string nome;
- int idade;
- int anoAtual = 2026;
- int result;
+int result1 = 4 + 4 * 2;
+int result2 = 4 + 2 / 2;
+int result3 = (4 + 4) * 2; 
 
- std::cout<<" Qual seu nome? "<<std::endl; 
- std::cin>>nome;
- std::cout<<" Qual sua idade? "<<std::endl; 
- std::cin>>idade; 
+std::cout<<result1<<std::endl;
+std::cout<<result2<<std::endl;
+std::cout<<result3<<std::endl; 
 
- result = anoAtual - idade; 
- std::cout<<result<<std::endl;
-
- std::cout<<"olá"<<nome<<"!!"<<std::endl;
- std::cout<<"você tem"<<idade<<"anos"<<std::endl;
- std::cout<<"você nasceu no ano"<<result; 
-
+ 
 
     return 0; 
 
