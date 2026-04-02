26e35bee302914a8d9b06a926d6bbbab1d1edd52
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 21:55:12 -0300

    String nome

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include<iostream>
-
+#include<string> 
 int main(){
 
-int num1, num2, soma; 
-std::cout<<" digite o valor de num 1: "<<"\n";
-std::cin>>num1;
+   std::string nome = "Alana";
+   std::string sobrenome = "Milk";
 
-std::cout<<" digite o valor de num 2: "<<"\n";
-std::cin>>num2; 
-//\n\n serve para dar espaço e organizar melhor 
-soma = num1 + num2; 
-
-
-std::cout<<" o resultado da soma é "<<soma<<"\n";
- 
-//\n serve para dar enter, igual ao endl, porem ocupa menos espaço 
+   std::cout<<nome + " " + sobrenome <<std::endl; 
+   
     return 0; 
 
 }
