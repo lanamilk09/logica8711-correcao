79f2b012c62ee61df94725d36aae4cf5b347ded1
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 21:34:17 -0300

    \n first step

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 int main(){
 
 int num1, num2, soma; 
-std::cout<<"\n\n o valor de num 1: "<<"\n";
+std::cout<<" digite o valor de num 1: "<<"\n";
 std::cin>>num1;
 
-std::cout<<"\n\n o valor de num 2: "<<"\n";
+std::cout<<" digite o valor de num 2: "<<"\n";
 std::cin>>num2; 
 //\n\n serve para dar espaço e organizar melhor 
 soma = num1 + num2; 
 
 
-std::cout<<"\n\n\n o resultado da soma é "<<soma<<"\n";
+std::cout<<" o resultado da soma é "<<soma<<"\n";
  
-
+//\n serve para dar enter, igual ao endl, porem ocupa menos espaço 
     return 0; 
 
 }
