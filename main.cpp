79ae8906095bacdc41f228af878532df4da66ff8
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-01 21:28:46 -0300

    \n

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 
 int main(){
 
-int result1 = 4 + 4 * 2;
-int result2 = 4 + 2 / 2;
-int result3 = (4 + 4) * 2; 
+int num1, num2, soma; 
+std::cout<<"\n\n o valor de num 1: "<<"\n";
+std::cin>>num1;
 
-std::cout<<result1<<std::endl;
-std::cout<<result2<<std::endl;
-std::cout<<result3<<std::endl; 
+std::cout<<"\n\n o valor de num 2: "<<"\n";
+std::cin>>num2; 
+//\n\n serve para dar espaço e organizar melhor 
+soma = num1 + num2; 
 
+
+std::cout<<"\n\n\n o resultado da soma é "<<soma<<"\n";
  
 
     return 0; 
