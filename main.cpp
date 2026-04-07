65fb15b0e453e370f2d47a77cc38211ea6b867f9
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-06 21:58:39 -0300

    contagem e troca

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ int main(){
  std::cout<<nome[0]<<std::endl;
  std::cout<<nome[2]<<std::endl;
  std::cout<<nome[nome.length() - 1]<<std::endl;
+ nome[0] = 'A';
+ std::cout<<nome<<std::endl; 
+ std::cout<<nome<<std::endl;
+ std::cout<<nome.at(0)<<std::endl;
+ nome.at(0) = 'H';
+ std::cout<<nome;
+
 
     return 0;
 }
