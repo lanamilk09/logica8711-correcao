667f15d290fc0a86a9ca1301149aa48e45c98a16
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-06 21:35:30 -0300

    0=[1]

--- main.cpp
+++ main.cpp
@@ -3,11 +3,9 @@
 
 int main(){
 
-   std::string nome = "Alana";
+  int Alana[5] = {10,20,30,40,50}; 
 
-   std::cout<<"nome tem: "<<nome.length()<<" letras."<<std::endl;
-   std::cout<<"nome tem: "<<nome.size()<<" letras."<<std::endl;
-
+  std::cout<<Alana[1]; 
 
 
     return 0;
