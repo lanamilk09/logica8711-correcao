678d378dc233c6580b30a8f2c7f03688ebb1bb69
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 19:29:02 -0300

    ai vai explodir

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 int main(){
 
 
-   for(int i = 1; i <= 10; i++){
+   for(int i = 10; i >= 0; i--){
 // <= 10 - o sistema vai parar de somar quando chegar a 10
-    std::cout<<" este é um loop!"<<std::endl; 
+    std::cout<<" a bomba irá explodir em..."<<i<<std::endl; 
 
    }
 
