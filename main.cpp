26c32cb3b7b56d334b757a66046327b255d38d31
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 19:22:34 -0300

    for and loop

--- main.cpp
+++ main.cpp
@@ -5,24 +5,11 @@
 int main(){
 
 
-    std::string user;
-    std::string senha; 
+   for(int i = 1; i <= 10; i++){
+// <= 10 - o sistema vai parar de somar quando chegar a 10
+    std::cout<<" este é um loop!"<<std::endl; 
 
-    std::cout<<" Digite seu usuário de acesso: " <<std::endl; 
-    std::cin>>user;
-
-    std::cout<<" Digite sua senha: "<<std::endl; 
-    std::cin>>senha; 
-
-if(senha=="1234567"){
-    std::cout<<" o usuário "<<user<<" esta logado com sucesso!!"<<std::endl;
-
-}else{
-     std::cout<<"usuário ou senha inválidos!"<<std::endl;
-}
-
-
-
+   }
 
     return 0;
 }
