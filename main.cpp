a51ffe88e654d2c7a9787e5d0b62f13834f683bb
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 19:45:20 -0300

    chromo and thread

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-#include<string>
+#include<thread> // para lidar com o tempo de execução
+#include<chrono>// para definir unidades de tempo (m/s segundos)
 
 
 int main(){
@@ -9,7 +10,13 @@ int main(){
 // <= 10 - o sistema vai parar de somar quando chegar a 10
     std::cout<<" a bomba irá explodir em..."<<i<<std::endl; 
 
-   }
+   
+   std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
+   // define em quanto tempo a contagem irá diminuindo, tipo 3 segundo para ir de 10 a 9 na contagem 
+   
+
+   }
+   
     return 0;
 }
