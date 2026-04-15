aafe282fede6e0a61a91b3f48b5eef2e6348c9b7
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 19:58:53 -0300

    tabuada feita com for

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 #include<iostream>
-#include<thread> // para lidar com o tempo de execução
-#include<chrono>// para definir unidades de tempo (m/s segundos)
 
 
 int main(){
 
+  int tabuada = 7; 
 
-   for(int i = 10; i >= 0; i--){
-// <= 10 - o sistema vai parar de somar quando chegar a 10
-    std::cout<<" a bomba irá explodir em..."<<i<<std::endl; 
+  for(int i = 0; i <= 10; i++){
+    std::cout<<" o resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
 
-   
-   std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-
-   // define em quanto tempo a contagem irá diminuindo, tipo 3 segundo para ir de 10 a 9 na contagem 
-   
-
-   }
-   
+  }
+     
     return 0;
 }
