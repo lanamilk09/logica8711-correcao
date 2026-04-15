2e422d450d84710824987d6527a0ea277d901c7e
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 20:20:10 -0300

    Switch

--- main.cpp
+++ main.cpp
@@ -3,12 +3,50 @@
 
 int main(){
 
-  int tabuada = 7; 
+ int opçao; 
 
-  for(int i = 0; i <= 10; i++){
-    std::cout<<" o resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+ std::cout<<"digite um valor para a opção: "<<std::endl;
+ std::cin>>opçao;
+ switch(opçao){
+  case 1:
 
-  }
+    std::cout<<"Domingo!"<<std::endl;
+    break;
+  case 2:   
+
+    std::cout<<"Segunda Feira!"<<std::endl;
+    break; 
+
+  case 3:
+
+  std::cout<<" Terça feira! "<<std::endl;
+  break;
+
+  case 4: 
+  std::cout<<"quarta feira!"<<std::endl;
+  break;
+
+  case 5: 
+  std::cout<<"quinta feira!"<<std::endl;
+  break;
+
+  case 6:
+
+  std::cout<<"sexta feira!"<<std::endl;
+  break; 
+ 
+ case 7: 
+ std::cout<<"sábado!"<<std::endl; 
+ break;
+
+
+
+
+  default:
+
+  std::cout<<" Número inválido!"<<std::endl;
+
+ }
      
     return 0;
 }
