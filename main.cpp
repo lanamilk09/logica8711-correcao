a282c67d7b823821d24bb4249c61cdb68566b731
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 21:37:32 -0300

    numbers for (int)

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 
 int main(){
- int soma = 0; 
-
- for(int i = 1; i <= 2; ++i){
-
-std::cout<<"externo: "<<i<<std::endl;
-
-    for(int j = 1; j <= 3; ++j){
-        std::cout<<"interno: "<<j<<std::endl;
-
-    }
+ int mynumbers[5] = {10, 20, 30, 40, 50,};
+ for(int num : mynumbers){
+      std::cout<<num<<std::endl;
  }
-
     
 
     return 0;
