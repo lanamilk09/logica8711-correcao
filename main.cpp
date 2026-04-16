9d69d7712f01edcdbc2b1fd6ec9c4a5847b7d6fe
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-15 21:12:54 -0300

    for soma

--- main.cpp
+++ main.cpp
@@ -1,52 +1,16 @@
-#include<iostream>
-
+#include <iostream>
 
 int main(){
+ int soma = 0; 
 
- int opçao; 
+ for(int i = 1; i <= 5; i++){
 
- std::cout<<"digite um valor para a opção: "<<std::endl;
- std::cin>>opçao;
- switch(opçao){
-  case 1:
-
-    std::cout<<"Domingo!"<<std::endl;
-    break;
-  case 2:   
-
-    std::cout<<"Segunda Feira!"<<std::endl;
-    break; 
-
-  case 3:
-
-  std::cout<<" Terça feira! "<<std::endl;
-  break;
-
-  case 4: 
-  std::cout<<"quarta feira!"<<std::endl;
-  break;
-
-  case 5: 
-  std::cout<<"quinta feira!"<<std::endl;
-  break;
-
-  case 6:
-
-  std::cout<<"sexta feira!"<<std::endl;
-  break; 
- 
- case 7: 
- std::cout<<"sábado!"<<std::endl; 
- break;
-
-
-
-
-  default:
-
-  std::cout<<" Número inválido!"<<std::endl;
-
+    soma = soma + i;
  }
-     
+ 
+ std::cout<<"soma é: "<<soma<<std::endl;
+
+    
+
     return 0;
-}
+ }
