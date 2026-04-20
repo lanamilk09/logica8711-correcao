a3e0df6863d5c6c67289966817584f8cb5fab3c1
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-20 18:39:05 -0300

    teste 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
+#include<chrono>
+#include<thread> 
 
 int main(){
- int mynumbers[5] = {10, 20, 30, 40, 50,};
-// for(int num : mynumbers){
-     // std::cout<<num<<std::endl;
-     for(int i = 0; i < 5; ++i){
-        std::cout<<mynumbers[i]<<std::endl;
-        // quando o i = 0 ele puxa o 10, quando o i se torna 1 ele puxa o 20, quando o i se torna 2 ele puxa o 30
-        
+
+   int opção;
+
+   std::cout<<"I choose you!: "<<std::endl;
+   std::cin>>opção;
+   switch (opção){
+
+   case 1:
+   std::cout<<" pikachu! "<<std::endl;
+   break;
+
+   case 2:
+
+   std::cout<<"Mewtwo!"<<std::endl;
+   break;
+
+   case 3: 
+
+   std::cout<<"Charizard!"<<std::endl;
+   break;
+
+   case 4 : 
+
+   std::cout<<" eevee! "<<std::endl;
+   break;
+
+   case 5:
+
+   std::cout<<" Bulbasaur! "<<std::endl;
+   break;
 
 
-     }
+
+
+
+
+
+
+     
  
     
-
+   }
     return 0;
  }
