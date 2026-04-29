7aefe574be44a8dc88bd5ef464bad092acbc8b08
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-28 21:11:38 -0300

    explanation myfunction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
 
-void minhafuncao(std::string nome){
-   std::cout<<nome<<std::endl;
+void minhafuncao(std::string nome, int idade){
+   std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos. "<<std::endl;
 }
 int main(){
-    minhafuncao ("Alana");
-  minhafuncao("Nome");
+    minhafuncao ("Alana", 16);
 
     return 0;
  }
