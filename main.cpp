e246cccf82acb7eb5d2a55581143a7b3589089c0
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-04-30 20:49:20 -0300

    teste if else

--- main.cpp
+++ main.cpp
@@ -2,26 +2,18 @@
 #include <string>
 
 int main(){
- //br]Amount: valor em reais ExchangeRate: Taxa de Câmbio usdAmount: Valor em dólares
- double brlAmount, exchangeRate, usdAmount;
+ int num;
 
-int choice;//choice: escolha
+std::cout<<"Digite um número: "<<std::endl;
+std::cin>>num;
 
-std::cout<<"--- Currency Converter: BRL to USD---"<<std::endl;
-
-std::cout<<"Enter the current exchange rate (how much is 1 dolar in Reais?): "<<std::endl;
-std::cin>>exchangeRate;
-
-std::cout<<"Enterthe amount in BRL(R$):"<<std::endl;
-std::cin>>brlAmount;
-
-if(brlAmount > 0 ){
-   usdAmount = brlAmount / exchangeRate;
-   std::cout<<"You will have $"<<usdAmount<<" US dollars. "<<std::endl;
-
-
-}
-
+ std::cout<<num%2<<std::endl;
+ if( num % 2 == 0){
+   std::cout<<"par"<<std::endl;
+ }else{ 
+   if( num % 2 <= 1 )
+   std::cout<<"impar"<<std::endl;
+ }
 
     return 0;
  }
