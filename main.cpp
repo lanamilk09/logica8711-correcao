c1257a69c8ce9b5bfd60d2d99a2cf0298da3b095
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-05-05 19:53:26 -0300

    getline

--- main.cpp
+++ main.cpp
@@ -2,15 +2,13 @@
 #include <string>
 
 int main(){
- int num;
 
-int totalseconds;
-std::cout<<"Enter total seconds: "<<std::endl;
-std::cin>>totalseconds;
-int minutes = totalseconds /60;
-int seconds = totalseconds %60;
+std::string nomeCompleto;
 
-std::cout<<"Time: "<<minutes<<" minutes and "<<seconds<<" seconds."<<std::endl;
+std::cout<<"Digite o nome completo do Aluno: "<<std::endl;
+std::getline(std::cin, nomeCompleto);
+
+std::cout<<nomeCompleto<<std::endl; 
 
     return 0;
  }
