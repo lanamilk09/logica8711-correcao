a83db4be5bb7b1356a0bda3a703d39ced1794e9d
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-05-12 20:57:16 -0300

    Calculadora funções

--- teste.cpp
+++ teste.cpp
@@ -1,49 +1,65 @@
 #include<iostream>
 
-int main(){
- int num1, num2, soma, subt, mult;
- int op;
- float div;
- std::cout<<"Digite aqui um valor para o numero 1: "<<std::endl;
- std::cin>>num1;
- std::cout<<"Digite aqui um valor para o numero 2: "<<std::endl;
- std::cin>>num2;
 
- soma = num1 + num2;
- subt= num1 - num2;
- mult= num1 * num2;
- div = num1 / num2;
- std::cout<<" -------- Escolha a operação -------"<<std::endl;
- std::cout<<" 1- soma ";
- std::cout<<" 2- subtração "; 
- std::cout<<" 3- nultiplicação  ";
-std::cout<<"4- divisão ";
-std::cin>>op;
-
-switch (op){
-    case 1:
-    soma = num1 + num2;
-    std::cout<<" O resultado da soma é: "<<soma<<std::endl;
-    break;
-    case 2 :
-     subt= num1 - num2;
-    std::cout<<" O resultado da subtração é: "<<subt<<std::endl;
-    break;
-    case 3 :
-    mult= num1 * num2;
-    std::cout<<" O resultado da multiplicação é: "<<mult<<std::endl;
-    break;
-    case 4:
-    div = num1 / num2;
-    std::cout<<" O resultado da divisão é: "<<div<<std::endl;
-    break;
-
-    default:
-    std::cout<<" Você não inseriu nenhum valor valido! "<<std::endl;
+float somar( float a, float b){
+    return a + b;
+}
+float subtrair (float a, float b){
+    return a - b;
+}
+float multiplicar (float a, float b){
+    return a * b;
 
 }
+float dividir (float a, float b){
+    if( b == 0){
+        std::cout<<" Nao é possivel dividir por 0!"<<std::endl;
+        return 0;
 
-    
+    }else{
+        return a/b; 
+    }
+}
+void coletarNumeros(float &a, float &b){
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>a;
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>b;
 
-    return 0;
+}
+void exibirResultado(float &a, float &b){
+    coletarNumeros(a,b);
+     int op; 
+     std::cout<<"Digite uma opcao:  "<<std::endl;
+      std::cout<<" 1- somar"<<std::endl;
+     std::cout<<" 2- subtrair "<<std::endl;
+      std::cout<<" 3- multiplicar "<<std::endl;
+       std::cout<<" 4- dividir "<<std::endl;
+       std::cin>>op;
+
+
+
+
+       switch(op){
+        case 1: 
+        std::cout<<" o resultado é: "<<somar(a,b)<<std::endl;
+        break;
+        case 2:
+        std::cout<<" O resultado é: "<<subtrair<<(a,b)<<std::endl;
+        break; 
+        case 3:
+        std::cout<<"o resultado é: "<<multiplicar(a,b)<<std::endl;
+        break;
+        case 4 : 
+        std::cout<<" O resultado é: "<<dividir(a,b)<<std::endl;
+        break;
+        default:
+        std::cout<<"Você digitou um valor inválido! "<<std::endl;
+       }
+}
+
+int main (){
+    float num1 = 0, num2 = 0;
+    exibirResultado(num1,num2);
+ return 0;
 }
