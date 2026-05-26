72d060eb8b8916c1cf69786b310a8420174b6c0c
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-05-26 19:35:48 -0300

    interpolação de variáveis

--- teste.cpp
+++ teste.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
+#include<format>
+#include<string>
 int main(){
-int numero; 
-int contador = 0; 
-while(true){
-   std::cout<<"digite um numero (0 para parar): "; 
-   std::cin>>numero; 
-   if(numero == 0){
-      break;
-   }
-   contador++;
-}
-std::cout<<"voce digitou"<<contador<<"numeros!"<<std::endl;
+
+std::string nome = "Eduardo"; 
+
+std::string mensagem = std::format("Olá,{}! voce tem {}anos.\n", nome); 
+std::cout<<mensagem; 
+
+
 return 0; 
 
 
