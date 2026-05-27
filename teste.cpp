38a8572186d11b2b0e9e7ef7338261b286f2ce37
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-05-26 21:35:45 -0300

    Uso de arrey

--- teste.cpp
+++ teste.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
+int encontrarMaior(int arr[], int tamanho){
+   int maior = arr[0]; 
 
-int main(){
-   int numeros [5];
-   int soma = 0 ; 
-   for(int i = 0; i <= 4; i++){
-       std::cout<<"Digite um numero: "<<(i + 1)<<": "; 
-       std::cin>>numeros[i];
-       soma += numeros[i];
+   for(int i = 1; i < tamanho; i++){
+      if(arr[i]>maior){
+         maior = arr[i];
+      }
    }
-
-
-   std::cout<<"soma: "<<soma<<std::endl; 
+   return maior; 
+}
+   int main(){
+       int numeros [5];
    
+   std::cout<<"Digite 5 numeros: "<<std::endl; 
+   for(int i = 0; i <= 4; i++){
+       std::cout<<"Digite um numero: "<<(i + 1)<<": ";  
+       std::cin>>numeros[i]; 
+   
+   }
+   std::cout<<"Maior numero: "<<encontrarMaior(numeros, 5)<<std::endl; 
+
+   return 0; 
 }
