039f357e9698ddd87f882fa2bcca45c937457f5e
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-05-28 19:23:52 -0300

    tuple2

--- teste.cpp
+++ teste.cpp
@@ -3,10 +3,28 @@
 #include<string>
 
 int main(){
-   std::tuple<int, std::string, double> aluno(1, "Alana", 9.5);
+  std::string nome; 
+  int idade; 
+  double altura; 
 
-   std::cout<<"ID: "<<std::get<0>(aluno)<<"\n";
-     std::cout<<"Nome: "<<std::get<1>(aluno)<<"\n";
-       std::cout<<"Nota: "<<std::get<2>(aluno)<<"\n";
+  std::cout<<"Digite o seu nome: "<<std::endl; 
+  std::getline(std::cin,nome); 
+
+  std::cout<<"Digite a sua idade: "<<std::endl; 
+  std::cin>>idade; 
+
+  std::cout<<"Digite sua altura: "<<std::endl; 
+  std::cin>>altura; 
+
+  std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
+
+
+  std::cout<<"\n-----Dados salvos na tupla-------"<<std::endl; 
+
+
+  std::cout<<"nome: "<<std::get<0>(pessoa)<<std::endl; 
+  std::cout<<"idade: "<<std::get<1>(pessoa)<<" anos. "<<std::endl; 
+  std::cout<<"Altura: "<<std::get<2>(pessoa)<<" m. "<<std::endl; 
+
 return 0; 
 }
