c21dbe8ccade0526f4158b9c59cc7a5fb8b21f61
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-02 19:30:18 -0300

    palindromo

--- teste.cpp
+++ teste.cpp
@@ -1,17 +1,37 @@
 #include<iostream>
-int somarVetor(int vetor[], int indice){
-  if(indice == 0){
-    return vetor [0];
+#include<string>
+#include<cctype>
 
+
+
+bool ehpalindromo(std:: string s){
+  int inicio = 0; 
+  int fim = s.length() - 1; 
+  while (inicio < fim){
+    if(s[inicio] != s[fim]){
+      return false; 
+    }
+    inicio++; 
+    fim--; 
   }
-  return vetor[indice] + somarVetor(vetor,indice -1);
+  return true; 
 
 }
 int main(){
-  int numeros [5] = {10,20,30,40,50}; 
-  std::cout<<"Soma recursiva: "<<somarVetor(numeros,4)<<std::endl; 
+
+  std::string palavra ; 
+  std::cout<<"Digite uma palavra: "<<std::endl; 
+  std::cin>>palavra; 
+
+  for(int i = 0; i < palavra.length(); i++){
+    palavra[i] = std::tolower(palavra[i]);
+  } 
+  if(ehpalindromo(palavra)){
+    std::cout<<" é palindromo! "<<std::endl; 
+  }else{
+    std::cout<<" Não é palindromo! "<<std::endl; 
+  }
   return 0; 
 }
-
  
 
