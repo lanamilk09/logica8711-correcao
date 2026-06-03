b57a3a6a695593e4b319db047a6c14077dc2530d
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-02 21:11:34 -0300

    struct pessoa

--- teste.cpp
+++ teste.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 
-void dividir(int a, int b, int* quociente, int* resto){
-  *quociente = a / b;
-  *resto = a % b; 
-
-}
+struct pessoa{
+  std::string nome ; 
+  int idade ; 
+  float altura; 
+}; 
 int main(){
-  int q, r; 
-  dividir(17, 5, &q, &r);
-  std::cout<<"Quociente: "<<q<<", Resto: "<<r<<std::endl; 
+   
+ pessoa p1 = { "Junior ", 20, 1.80};
+ pessoa p2 = {"Carlos", 30, 1.70};
+
+ std::cout<<p2.nome<<" tem "<<p2.idade<<" anos e "<<p2.altura<<" de altura "<<std::endl; 
+  
   return 0; 
 }
 
