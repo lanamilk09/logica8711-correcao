8568d47c5973343526cd2168a82f60b3367d5f98
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-11 19:28:51 -0300

    torre de Hanoi

--- teste.cpp
+++ teste.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 #include<string> 
-int main(){
-  int idade = 38; 
-  std::string resultado = (idade >= 18 ) ? "Maior de idade! " : "menor de idade! "; 
-  std::cout<<resultado<<std::endl; 
+void torreDeHanoi(int n, std::string origem, std::string destino, std::string auxiliar){
+if(n == 1){
+  std::cout<<"Mover disco "<<origem<<" para "<<destino<<std::endl; 
+  return; 
+}  
+torreDeHanoi(n - 1, origem, auxiliar, destino); 
+std::cout<<"Mover disco "<<n<<" de "<<origem<<" para  "<<destino<<std::endl; 
+torreDeHanoi(n - 1, auxiliar, destino, origem); 
 
-} 
+}
+int main(){
+  int numDiscos; 
+  std::cout<<"Quantos Discos: "; 
+  std::cin>>numDiscos; 
+
+  std::cout<<"\n sequencia de movimentos: "<<std::endl; 
+  torreDeHanoi(numDiscos, " A ", " C ", " B "); 
+  std::cout<<"\n total de movimentos "<<( 1 << numDiscos) - 1<<std::endl; 
+  return 0; 
+
+}
