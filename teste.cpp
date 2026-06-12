f422a96f875d7bc82686ab3dc4df09f3ba56ed87
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-11 21:00:55 -0300

    encontrar maior

--- teste.cpp
+++ teste.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 
-int mdc(int a, int b){
-  if(b == 0) return a; 
-  return mdc(b, a % b); 
-
-} int main(){
-  std::cout<<"MDC(48,18) = "<<mdc<<(48,18)<<std::endl; 
-  std::cout<<"MDC(100,50) = "<<mdc<<(100,50)<<std::endl; 
+int encontrarMaior(int* arr, int tamanho){
+  int maior = arr[0];
+  for(int i = 1; i < tamanho; i++){
+    if(arr[i]>maior){
+      maior = arr[i];
+    }
+  }
+  return maior; 
+}
+int main(){
+  int arr[] = {3,7,2,9,1,5};
+  std::cout<<"maior elemento: "<<encontrarMaior(arr, 6)<<std::endl; 
   return 0; 
 }
