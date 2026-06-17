865ff12729c3b1f574c702aa77b8273380280853
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-17 20:19:32 -0300

    placar

--- teste.cpp
+++ teste.cpp
@@ -1,46 +1,40 @@
 #include<iostream>
 #include<string>
 
-struct jogador{
-  std::string nome; 
-  std::string time; 
-  int gols;
-};
+int main(){
+  std::string time1, time2; 
+  int gols1, gols2; 
 
-int main (){
-  jogador jogadores[5]; 
-
-  std::cout<<" ====== COPA - Ranking de goleadores ====== "<<std::endl; 
-  std::cout<<std::endl;
-
-  for(int i = 0; i < 5; i++){
-    std::cout<<"jogador "<<(i + 1 )<<":"<<std::endl; 
-    std::cout<<"nome: "; 
-    std::cin>>jogadores[i].nome;
-    std::cout<<"time: ";
-    std::cin>>jogadores[i].time;
-    std::cout<<"Gols: ";
-    std::cin>>jogadores[i].gols;
-    std::cout<<std::endl; 
-  }
-  std::cout<<" ====== RAnking ======"<<std::endl; 
+  std::cout<<"====== comparador de placar ========"<<std::endl; 
   std::cout<<std::endl; 
 
-  for(int i = 0; i < 5 - 1; i++){
-    for(int j = 0; j < 5 - 1; j++){
-      if(jogadores[i].gols < jogadores[j+1].gols){
-        jogador temp = jogadores[j];
-        jogadores[j] = jogadores[j + 1]; 
-        jogadores[ j + 1] = temp; 
-      }
-    }
-  }
+  std::cout<<" Time 1: ";
+  std::cin>>time1; 
+  std::cout<<"gols time 1: ";
+  std::cin>>gols1; 
 
-for(int i = 0; i < 5; i++){
-  std::cout<<( i + 1)<<"--"<<jogadores[i].nome
-           <<"("<<jogadores[i].time<<")-"
-           <<jogadores[i].gols<<"gols"<<std::endl; 
+  std::cout<<" Time 2: "; 
+  std::cin>>time2; 
+  std::cout<<"gols time 2: ";
+  std::cin>>gols2; 
+
+  std::cout<<std::endl; 
+  std::cout<<"========== resultado ========="; 
+  std::cout<<time1<<" "<<gols1<<" x "<<gols2<<" "<<time2<<std::endl;
+  std::cout<<std::endl; 
+
+if( gols1 > gols2){
+  std::cout<<"O time 1 ganhou! "<<std::endl; 
+
 
 }
-return 0; 
+else if(gols2 > gols1){
+  std::cout<<" O time 2 ganhou! "<<std::endl;  
+
+}
+else{
+  std::cout<<" empate! "<<std::endl; 
+}
+  return 0; 
+
 }
