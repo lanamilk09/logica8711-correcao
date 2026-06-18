e778da97413ba57a5d59838896932a4bbd29131e
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-17 21:39:19 -0300

    media

--- teste.cpp
+++ teste.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
-#include<string>
-struct CampeonatosCopa{
-  int ano; 
-  std::string campeao;
 
-};
 
 int main(){
-  CampeonatosCopa copa[5] = {
-    {2022, "argentina"},
-    {2018, "França"},
-    {2014, "alemanha"},
-    {2010, "espanha"},
-    {2006, "italia"}
-  };
-  std::cout<<"=== Campeoes da Copa ==="<<std::endl; 
+  int jogos; 
+
+  std::cout<<"==== Calculador de publico ======"<<std::endl; 
   std::cout<<std::endl; 
 
-for(int i = 0; i < 5; i++ ){
-  std::cout<<copa[i].ano<<" - Campeao: "<<copa[i].campeao<<std::endl; 
-}
- 
-  return 0; 
+  std::cout<<"Quantos Jogos? ";
+  std::cin>>jogos; 
 
+  int somaPublico = 0; 
+
+  for(int i = 1; i <= jogos; i++ ){
+    int publico; 
+    std::cout<<"publico jogo"<<i<<": ";
+    std::cin>>publico; 
+    somaPublico += publico; 
+
+  }
+  int media = somaPublico / jogos; 
+  std::cout<<std::endl; 
+  std::cout<<"==== Resultado ======"<<std::endl; 
+  std::cout<<" media de publico: "<<media<<" pessoas"<<std::endl; 
+  std::cout<<"total de publico: "<<somaPublico<<" pessoas "<<std::endl; 
+
+  return 0; 
 }
+
