289d407a0b6e6441535fc17292160fe18223220f
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-23 20:53:48 -0300

    pares e impares

--- teste.cpp
+++ teste.cpp
@@ -2,25 +2,23 @@
 
 int main(){
  int numeros[] = {1,2,3,2,4,5,3,6,};
- int tamanho = 8; 
- std::cout<<"=== Numeros duplicados ==="<<std::endl; 
+ int tamanho = 10; 
+ 
+ std::cout<<" ==== Pares e impares ====="<<std::endl; 
  std::cout<<std::endl; 
+ std::cout<<"Pares: "; 
 
- std::cout<<"Array: "; 
  for(int i = 0; i < tamanho; i++){
-  std::cout<<numeros[i]<<" "; 
+  if(numeros[i] % 2 == 0){
+    std::cout<<numeros[i]<<" "; 
 
-
+  }
  }
- std::cout<<std::endl; 
-
- std::cout<<" Numeros duplicados: "<<std::endl; 
+ std::cout<<std::endl;
  for(int i = 0; i < tamanho; i++){
-  for(int j = i + 1; j < tamanho; j++){
-    if(numeros[i] == numeros[j]){
-      std::cout<<numeros[i]<<" aparece mais de uma vez! "<<std::endl; 
-      break;
-    }
+  if(numeros[i] % 2 != 0){
+    std::cout<<numeros[i]<<" "; 
+
   }
  }
   
