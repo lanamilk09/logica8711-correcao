72e1b88c98ecc1d94f0ae429c1ae8272836ab835
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-23 20:13:15 -0300

    Array numeros duplicados

--- teste.cpp
+++ teste.cpp
@@ -1,61 +1,28 @@
 #include<iostream>
 
 int main(){
-  int fila[10];
-  int inicio = 0; 
-  int fim = 0; 
-  int topo = -1; 
-  int opcao; 
-  std::cout<<"==== Fila com Menu ======"<<std::endl; 
-  while(true){
-    std::cout<<std::endl; 
-    std::cout<<"1.Empilhar"<<std::endl; 
-    std::cout<<"2.Desempilhar"<<std::endl; 
-    std::cout<<"3. Exibir pilha"<<std::endl; 
-    std::cout<<"Sair"<<std::endl; 
+ int numeros[] = {1,2,3,2,4,5,3,6,};
+ int tamanho = 8; 
+ std::cout<<"=== Numeros duplicados ==="<<std::endl; 
+ std::cout<<std::endl; 
 
-    std::cout<<"Escolha: "<<std::endl; 
-    std::cin>>opcao; 
+ std::cout<<"Array: "; 
+ for(int i = 0; i < tamanho; i++){
+  std::cout<<numeros[i]<<" "; 
 
 
-    if(opcao ==1){
-      if(fim < 10){
-        int valor; 
-        std::cout<<"Digite o valor: ";
-        std::cin>>valor; 
-        fila[fim] = valor; 
-        fim++; 
-        std::cout<<"Enfileirado!"<<std::endl;
+ }
+ std::cout<<std::endl; 
 
-      }else{
-        std::cout<<"Fila Cheia!! "<<std::endl; 
-
-      }
-    }else if (opcao == 2){
-      if(inicio < fim){
-        std::cout<<"Removido: "<<std::endl; 
-        inicio++; 
-      }else{
-        std::cout<<"Fila VAzia! "<<std::endl; 
-      }
-
-    } else if(opcao == 3){
-      if(inicio < fim){
-        std::cout<<"Fila: ";
-        for(int i = inicio; i < fim; i++){
-          std::cout<<fila[i]<<" "; 
-
-        }
-        std::cout<<std::endl; 
-      }else{
-        std::cout<<"Fila Vazia! "<<std::endl; 
-      }
-    }
-    else if(opcao == 4){
-      std::cout<<"Saindo..."<<std::endl; 
+ std::cout<<" Numeros duplicados: "<<std::endl; 
+ for(int i = 0; i < tamanho; i++){
+  for(int j = i + 1; j < tamanho; j++){
+    if(numeros[i] == numeros[j]){
+      std::cout<<numeros[i]<<" aparece mais de uma vez! "<<std::endl; 
       break;
     }
-}
+  }
+ }
   
   return 0; 
   }
