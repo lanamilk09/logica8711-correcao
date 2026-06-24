5aac762f0ac61cd82222ea75005ac735c09856b3
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-06-23 21:07:44 -0300

    array find spot

--- teste.cpp
+++ teste.cpp
@@ -1,26 +1,38 @@
 #include<iostream>
 
 int main(){
- int numeros[] = {1,2,3,2,4,5,3,6,};
- int tamanho = 10; 
- 
- std::cout<<" ==== Pares e impares ====="<<std::endl; 
+ int numeros[] = {10,20,30,40,50,60,70,80};
+ int tamanho = 8; 
+ int buscado; 
+
+ std::cout<<"==== Buscar Numero ======"<<std::endl; 
  std::cout<<std::endl; 
- std::cout<<"Pares: "; 
+
+ std::cout<<"Array: "; 
+ for(int i = 0; i < tamanho; i++){
+  std::cout<<numeros[i]<<" "; 
+
+ }
+ std::cout<<std::endl; 
+
+ std::cout<<"Digite o numero a buscar: "<<std::endl; 
+ std::cin>>buscado;
+ int posicao = -1; 
 
  for(int i = 0; i < tamanho; i++){
-  if(numeros[i] % 2 == 0){
-    std::cout<<numeros[i]<<" "; 
-
+  if(numeros[i] == buscado){
+    posicao = i; 
+    break; 
   }
  }
- std::cout<<std::endl;
- for(int i = 0; i < tamanho; i++){
-  if(numeros[i] % 2 != 0){
-    std::cout<<numeros[i]<<" "; 
+std::cout<<std::endl; 
+if(posicao != -1){
+  std::cout<<" Encontrado na posição "<<posicao<<std::endl; 
 
-  }
- }
+}else{
+  std::cout<<"Não encontrado! "<<std::endl; 
+}
+
   
   return 0; 
   }
