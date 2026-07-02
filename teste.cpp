cc5ab68faa5a65a9908522c0aca02cfca8543de1
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-02 20:11:00 -0300

    Tamanho do Vetor

--- teste.cpp
+++ teste.cpp
@@ -5,27 +5,18 @@
 int main(){
     std::vector<int>numeros; 
 
-    std::cout<<"--- VECTOR (arraylist em C++) ---"<<std::endl; 
-
-    numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
-
-    std::cout<<"Numeros: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
+    std::cout<<"--- 2. Tamanho do Vector ---"<<std::endl; 
     std::cout<<std::endl; 
-
-    std::cout<<"Tamanho: "<<numeros.size()<<std::endl; 
-    numeros.pop_back(); 
     
+    numeros.push_back(5);
+    numeros.push_back(10);
+    numeros.push_back(15); 
 
-    std::cout<<"Apó remover o ultimo: "; 
+    std::cout<<"Tamanho do Vector: "<<numeros.size()<<std::endl;
+    std::cout<<"Elementtos: "; 
     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-
-    }
+        std::cout<<numeros[i]<<std::endl; 
+    } 
 
 return 0; 
 }
