dfdd9c057fba1108bd46f2eeccbb809f8edc9561
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-02 20:24:57 -0300

    Acessar Elemento ( Vector )

--- teste.cpp
+++ teste.cpp
@@ -5,18 +5,24 @@
 int main(){
     std::vector<int>numeros; 
 
-    std::cout<<"--- 2. Tamanho do Vector ---"<<std::endl; 
-    std::cout<<std::endl; 
+    std::cout<<"--- Acessar Elemento ---"<<std::endl; 
+   
     
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(15); 
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300); 
+    numeros.push_back(400); 
 
-    std::cout<<"Tamanho do Vector: "<<numeros.size()<<std::endl;
-    std::cout<<"Elementtos: "; 
+    std::cout<<"Vector: "<<std::endl; 
     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<std::endl; 
-    } 
+        std::cout<<numeros[i]<<" "; 
+    }
+    std::cout<<std::endl; 
+
+    std::cout<<"Elemento na posiçao 0: "<<numeros[0]<<std::endl; 
+    std::cout<<"Elemento na posição 2: "<<numeros[2]<<std::endl; 
+    std::cout<<"Ultimo Elemento: " <<numeros[numeros.size() - 1]<<std::endl; 
+
 
 return 0; 
 }
