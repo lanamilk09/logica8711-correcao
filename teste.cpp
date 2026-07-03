8566acdb57798f6dde748b08376b6b2d28aeaca3
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-02 21:36:36 -0300

    Limpar Vector

--- teste.cpp
+++ teste.cpp
@@ -5,25 +5,23 @@
 int main(){
     std::vector<int>numeros; 
 
-    std::cout<<"--- Acessar Elemento ---"<<std::endl; 
+    std::cout<<"---Limpar Vector ---"<<std::endl; 
+
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+   
+
+    std::cout<<" Tamanho Antes: "<<numeros.size()<<std::endl; 
+    numeros.clear();
+
+    std::cout<<"Tamanho Depois: "<<numeros.size()<<std::endl; 
+    std::cout<<"VEctor Vazio? "<<(numeros.empty() ? " Sim " : " Não ")<<std::endl; 
+   
+
    
     
-    numeros.push_back(100);
-    numeros.push_back(200);
-    numeros.push_back(300); 
-    numeros.push_back(400); 
-
-    std::cout<<"Vector: "<<std::endl; 
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" "; 
-    }
-    std::cout<<std::endl; 
-
-    std::cout<<"Elemento na posiçao 0: "<<numeros[0]<<std::endl; 
-    std::cout<<"Elemento na posição 2: "<<numeros[2]<<std::endl; 
-    std::cout<<"Ultimo Elemento: " <<numeros[numeros.size() - 1]<<std::endl; 
-
-
+   
 return 0; 
 }
 
