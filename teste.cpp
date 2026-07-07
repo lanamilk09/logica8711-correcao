9ee9689495117b5d2d8de1082d1ceec3f394ed5e
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-07 20:45:45 -0300

    Adicionar do Usuário Vector 2

--- teste.cpp
+++ teste.cpp
@@ -3,37 +3,25 @@
 
 
 int main(){
+    
     std::vector<int> numeros; 
-    std::cout<<"Buscar Elemento: "<<std::endl; 
-   
+    int quantidade; 
+    std::cout<<"Adicionar do Usuario "<<std::endl; 
 
     
-    numeros.push_back(100);
-    numeros.push_back(200);
-    numeros.push_back(300);
-    numeros.push_back(400);
-    //numeros.push_back(15);
-    //numeros.push_back(5);
-    //numeros.push_back(15);
+    std::cout<<"Quantos números? "<<std::endl; 
+    std::cin>>quantidade; 
+    
+     for(int i = 0; i < quantidade; i++){
+        int numero; 
+        std::cout<<"Digite um numero: "<<(i + 1)<<": ";
+        std::cin>>numero; 
+        numeros.push_back(numero);
+        }
 
-    std::cout<<"Vector: ";
+    std::cout<<"Numeros Adicionados: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
-    }
-    int buscado = 300; 
-    int posicao = -1;
-
-    for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] == buscado){
-            posicao = i; 
-            break; 
-        }
-    }
-    if(posicao != -1){
-        std::cout<<"Número "<<buscado<<"Encontrado na posição: "<<posicao<<std::endl; 
-
-    }else{
-        std::cout<<"Numero não encontrado! "<<std::endl; 
     }
 
    
