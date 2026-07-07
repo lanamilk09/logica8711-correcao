35563141a6d215ff1eeb7794c4855174efc7d2aa
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-07 19:26:12 -0300

    Soma Vector

--- teste.cpp
+++ teste.cpp
@@ -4,35 +4,27 @@
 
 int main(){
     std::vector<int>numeros; 
+    std::cout<<"Soma Total: "<<std::endl; 
+   
 
-    std::cout<<"---Limpar Vector ---"<<std::endl; 
-
-    numeros.push_back(15);
-    numeros.push_back(8);
-    numeros.push_back(25);
-    numeros.push_back(3);
-    numeros.push_back(42);
     
-     std::cout<<"Vector: ";
-     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" "; 
-     }
-     std::cout<<std::endl; 
-     int maior = numeros[0];
-     int menor = numeros[0]; 
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
+    numeros.push_back(50);
 
-     for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] > maior){
-            maior = numeros[i];
-        }
-        if(numeros[i] < menor){
-            menor = numeros[i];
-        }
-     }
-     std::cout<<"Maior: "<<maior<<std::endl; 
-     std::cout<<"Menor: "<<menor<<std::endl; 
+    std::cout<<"Vector: ";
+    for(int i = 0; i < numeros.size(); i++){
+        std::cout<<numeros[i]<<" ";
+    }
+    int soma = 0; 
+    for(int i = 0; i < numeros.size(); i++){
+        soma += numeros[i];
 
-    // std::cout<<"fazendo um teste!!";
+    }
+    std::cout<<"\nSoma: "<<soma<<std::endl;
+
 
    
 return 0; 
