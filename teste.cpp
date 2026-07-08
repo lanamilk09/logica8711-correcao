71546aaf710f4b1c02ce57402814fbec59161bd7
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-07 21:36:45 -0300

    Vector invertido

--- teste.cpp
+++ teste.cpp
@@ -5,24 +5,31 @@
 int main(){
     
     std::vector<int> numeros; 
-    int quantidade; 
-    std::cout<<"Adicionar do Usuario "<<std::endl; 
 
     
-    std::cout<<"Quantos números? "<<std::endl; 
-    std::cin>>quantidade; 
-    
-     for(int i = 0; i < quantidade; i++){
-        int numero; 
-        std::cout<<"Digite um numero: "<<(i + 1)<<": ";
-        std::cin>>numero; 
-        numeros.push_back(numero);
-        }
 
-    std::cout<<"Numeros Adicionados: ";
+
+    numeros.push_back(10); 
+    numeros.push_back(20); 
+    numeros.push_back(30); 
+    numeros.push_back(40); 
+    numeros.push_back(50); 
+
+    std::cout<<"Original: "; 
     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
+        std::cout<<numeros[i]<<std::endl; 
     }
+
+    std::cout<<std::endl; 
+    std::cout<<"Invertido: "; 
+    for(int i = numeros.size() -1; i >= 0; i--){
+        std::cout<<numeros[i]<<" "; 
+
+    }
+    std::cout<<std::endl; 
+
+    
+    
 
    
 return 0; 
