aa73bfe626521bc450959104c953f3a51d1f68c5
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-07-14 20:20:45 -0300

    const raio

--- teste.cpp
+++ teste.cpp
@@ -1,18 +1,21 @@
 #include<iostream>  
-#include<cmath>
 
 
-int calcularCubo(int num){
-    return static_cast<int>(std::pow(num,3)); 
-
-}
 int main(){
-    int a; 
-    std::cout<<"Digite um número inteiro: ";
-    std::cin>>a; 
-    int cubo = calcularCubo(a); 
-    std::cout<<" O cubo de "<<a<<" é: "<<cubo<<std::endl; 
+ 
+const double pi = 3.14;
+float raio; 
+ std::cout<<" Digite o valor do Raio: "<<std::endl; 
+ std::cin>>raio; 
+
+ int area; 
+ area = pi * (raio*raio);
+ std::cout<<" O valor da área do círculo é de: "<<area<<std::endl; 
+
  return 0; 
+
+
+
 }
      
 
